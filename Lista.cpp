c424a5b7c124ac986a05347c55f9315d2ae32ec5
HENSLEY RODRIGUEZ <papirazi14@hotmail.com>
2012-03-27 02:03:14 -0100

    Agregado del metodo remover

--- Lista.cpp
+++ Lista.cpp
@@ -30,5 +30,25 @@ void Lista::agregar(Objeto* objeto){
 }
 
 void Lista::remover(int key){
+	if(getPrimero()==NULL){
+		return false;
+	}else{
+		Objeto *temporal = this->primero
+		Objeto *temporalDos = NULL;
+		while (temporal!=NULL){
+			if(key == temporal->getKey()){
+				if(temporalDos== NULL){
+					primero = temporal ->getSiguiente();
+				}else{
+					temporalDos ->setSiguiente(temporal->getSiguiente());
+					if(temporal->getSiguiente() == NULL){
+						ultimo = temporalDos;
+					}
+				}
+			}
+			temporalDos = temporal;
+			temporal = temporal->getSiguiente();
+		}
+	}
 
 }
